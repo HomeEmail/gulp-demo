f5ed672400c5dec485b4cd2a5388d262707d774d
ivan <591694604@qq.com>
2016-07-06 18:20:01 +0800

    添加 html里图片版本控制 修复 任务同步执行bug

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,8 @@ var notify = require('gulp-notify');//提示信息
 var rev = require('gulp-rev');  //- 对文件名加MD5后缀
 var revCollector = require('gulp-rev-collector'); //- 路径替换
 var less = require('gulp-less'); //less处理
-var fs= require('fs');
+var base64 = require('gulp-base64');
+//var fs= require('fs');
 var runSequence=require('run-sequence');//按指定顺序运行任务
 
 var res = {
@@ -22,44 +23,68 @@ var res = {
 
 //对于img,生成rev-manifest.json文件
 gulp.task('img-task',function(){
-	gulp.src(res.imgFiles)
+	return gulp.src(res.imgFiles)
 		.pipe(gulp.dest('./dest/res/images'))
 		.pipe(rev())
 		.pipe(rev.manifest())
-		.pipe(gulp.dest('./res/temp/rev-img'));
+		.pipe(gulp.dest('./res/temp/rev-img'))
+	;
 });
 
 //less生成css和压缩,生成rev-manifest.json文件
 gulp.task('css-task',function(){
 	
-	gulp.src(res.lessFiles)
+	return gulp.src(res.lessFiles)
 		.pipe(less())//less处理
 		//.pipe(concat('all.min.css'))//合并后的文件名
 		.pipe(minifyCss())//压缩CSS为一行；
 		.pipe(gulp.dest('./res/temp/rev-less'))//输出文件
-		;
+	;
 });
+
+//base64 应用于css
+gulp.task('css-base64', function () {
+	return gulp.src('./res/temp/rev-less/*.css')
+		.pipe(base64({
+			baseDir: 'res/images',
+			extensions: ['svg', 'png', /\.jpg#datauri$/i],//匹配
+			exclude:    [/\.utvgo\.(com|net)/, '--live.jpg'],//不匹配
+			maxImageSize: 4*1024, // bytes
+			debug: true
+		}))
+		.pipe(gulp.dest('./res/temp/css-base64'))
+	;
+})
+
 //将图片md5应用于css 图片路径里
-gulp.task('rev-img',function(){
-	gulp.src(['res/temp/rev-img/*.json','./res/temp/rev-less/**.css'])
+gulp.task('rev-img-css',function(){
+	return gulp.src(['res/temp/rev-img/*.json','./res/temp/css-base64/*.css'])
 		.pipe(revCollector())
-		.pipe(gulp.dest('./dest/res/css'))
-		;
+		.pipe(gulp.dest('./res/temp/css-img'))
+	;
 });
 gulp.task('rev-css-manifest',function(){
-	gulp.src(['./dest/res/css/**.css'])
+	return gulp.src(['./res/temp/css-img/*.css'])
+		.pipe(gulp.dest('./dest/res/css'))
 		.pipe(rev())//文件名加md5后缀
 		.pipe(rev.manifest())//生成一个rev-manifest.json
 		.pipe(gulp.dest('./res/temp/rev-css'));//- 将 rev-manifest.json 保存到 rev-css 目录
-		;
+	;
 })
 
 //将css 文件md5 应用于html的css路径里
 gulp.task('rev-css',function(){
-	gulp.src(['res/temp/rev-css/*.json','./*.html'])
+	return gulp.src(['res/temp/rev-css/*.json','./*.html'])
 		.pipe(revCollector())
 		.pipe(gulp.dest('./res/temp/css-html'))
-		;
+	;
+});
+//将img 文件md5 应用于html的img路径里
+gulp.task('rev-img-html',function(){
+	return gulp.src(['res/temp/rev-img/*.json','./res/temp/css-html/*.html'])
+		.pipe(revCollector())
+		.pipe(gulp.dest('./res/temp/img-html'))
+	;
 });
 
 
@@ -69,7 +94,7 @@ gulp.task('lint',function(){
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'))
 		//.pipe(notify({message:'lint task ok'}))
-		;
+	;
 });
 //压缩js库
 gulp.task('js-lib',function(){
@@ -81,11 +106,11 @@ gulp.task('js-lib',function(){
 		.pipe(rev.manifest())
 		.pipe(gulp.dest('./res/temp/rev-js-lib'))
 		//.pipe(notify({message:'js-lib task ok'}))
-		;
+	;
 });
 //讲js lib文件md5应用于html的js路径里
 gulp.task('res-js-lib',function(){
-	gulp.src(['res/temp/rev-js-lib/*.json','./res/temp/css-html/*.html'])
+	return gulp.src(['res/temp/rev-js-lib/*.json','./res/temp/img-html/*.html'])
 	.pipe(revCollector())
 	.pipe(gulp.dest('./res/temp/js-lib-html'))
 
@@ -102,15 +127,16 @@ gulp.task('js-own',function(){
 		.pipe(rev.manifest())
 		.pipe(gulp.dest('./res/temp/rev-js-own'))
 		//.pipe(notify({message:'js-own task ok'}))
-		;
+	;
 
 });
 
 //讲js 文件md5应用于html的js路径里
 gulp.task('res-js-own',function(){
-	gulp.src(['res/temp/rev-js-own/*.json','./res/temp/js-lib-html/*.html'])
-	.pipe(revCollector())
-	.pipe(gulp.dest('./dest'))
+	return gulp.src(['res/temp/rev-js-own/*.json','./res/temp/js-lib-html/*.html'])
+		.pipe(revCollector())
+		.pipe(gulp.dest('./dest'))
+	;
 
 })
 
@@ -120,19 +146,29 @@ gulp.task('build',function(){
 	runSequence(
 		'img-task' //// ... just do this
 		,'css-task'
-		,'rev-img'
+		,'css-base64'
+		,'rev-img-css'
 		,'rev-css-manifest'
 		,'rev-css'
-		,'lint'
+		,'rev-img-html'
+		//,'lint'
 		,'js-lib'
 		,'res-js-lib'
 		,'js-own'
 		,'res-js-own'
 
+
+
 		//,['css-task','rev-css'] // These 2 can be done in parallel
 	);
 });
 
+gulp.task('auto', function() {
 
+	gulp.watch(res.lessFiles, ['css-task']);
+
+
+
+});
 
 gulp.task('default',['build']);
